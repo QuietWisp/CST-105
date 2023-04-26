361378d47c8d37285561b79c31a7aa75c1ef23b4
Austin Fisher <46381860+QuietWisp@users.noreply.github.com>
2023-04-26 13:12:40 -0700

    Exercise 4 Runner

---
+++ Exercise 4/Runner.java
@@ -0,0 +1,17 @@
+public class Runner 
+{
+	public static int Max(int num1, int num2)
+	{
+		return Math.max(num1, num2);
+	}
+	
+	public static int Min(int num1, int num2)
+	{
+		return Math.min(num1, num2);
+	}
+	
+	public static int Avg(int total, int amount)
+	{
+		return total / amount;
+	}
+}
