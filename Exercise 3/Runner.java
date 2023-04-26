87069f80e968751b3fca53402430d01a2584b7e1
Austin Fisher <46381860+QuietWisp@users.noreply.github.com>
2023-04-26 13:09:49 -0700

    Exercise 3 Runner

---
+++ Exercise 3/Runner.java
@@ -0,0 +1,19 @@
+
+public class Runner 
+{
+	public int Compare(int num1, int num2)
+	{
+		if (num1 > num2)
+		{
+			return -1;
+		}
+		
+		if (num1 == num2)
+		{
+			return 0;
+		}
+		
+		return 1;
+	
+	}
+}
